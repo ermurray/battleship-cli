209ecf4b60faa5ac12dd1fc419ed23f71a4ef91c
Eric Murray <ermurray31@gmail.com>
2021-06-02 10:31:02 -0600

    build out menu

--- game.js
+++ game.js
@@ -1,34 +1,44 @@
 const inquirer = require('inquirer');
 
+const mainMenu = {
+  type: 'list',
+  name: 'main',
+  message: 'What do you want to do?',
+  choices: [
+    'Play Battleship',
+    'instructions',
+    new inquirer.Separator(),
+    'Exit Game',
+  ],
+};
+const opponentSelection =  {
+    type: 'list',
+    name: 'opponent',
+    message: 'Two player local or play against CPU?',
+    choices: [
+      '2Player', 
+      'CPU', 
+      new inquirer.Separator(),
+      'Main Menu'
+    ]
+  };
+
+
 inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'main',
-      message: 'What do you want to do?',
-      choices: [
-        'Play Battleship',
-        'instructions',
-        new inquirer.Separator(),
-        'Exit Game',
-        // {
-        //   name: 'Contact support',
-        //   disabled: 'Unavailable at this time',
-        // },
-        // 'Talk to the receptionist',
-      ],
-    },
-    {
-      type: 'list',
-      name: 'opponent',
-      message: 'Two player local or play against CPU?',
-      choices: ['2Player', 'CPU', 'Exit Game'],
-      filter: function (val) {
-        return val.toLowerCase();
-      },
-    },
-  ])
+  .prompt(mainMenu)
   .then((answers) => {
-    console.log(JSON.stringify(answers, null, '  '));
+    switch (answers.main) {
+      case 'Play Battleship':
+        console.log('Play battleship move to next menu');
+        inquirer.prompt(opponentSelection)
+        break;
+      case 'instructions':
+        console.log('instructions here prompt to return to main menu');
+        break;
+      default:
+        process.exit();
+    }
+    
+    // console.log(JSON.stringify(answers, null, '  '));
   });
 
