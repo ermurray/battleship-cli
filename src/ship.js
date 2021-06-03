d200d44e08383c44ca005a4e1ae31c040728e19f
Eric Murray <ermurray31@gmail.com>
2021-06-03 14:44:48 -0600

    add ship class

---
+++ src/ship.js
@@ -0,0 +1,15 @@
+export default class ship {
+  constructor (shipType) {
+    this.name = shipTyper.name,
+    this.icon = shipType.icon,
+    this.size = shipType.size,
+    this.sunk = false
+  }
+
+  placeShip(coordinates, direction){
+    //coordinates + this.length by the direction 
+    // then check if valid placement
+    //if valid replace spaces on board with ship icons
+  }
+
+}
---
+++ utils/shipTypes.js
@@ -0,0 +1,29 @@
+const shipType = {
+
+  battleShip : {
+    name: 'Battle Ship',
+    size : 4,
+    icon : 'B'
+  },
+  crusier : {
+    name: 'Crusier',
+    size: 3,
+    icon: 'C'
+  },
+  submarine : {
+    name: 'Submarine',
+    size: 3,
+    icon: 'S',
+  },
+  destroyer: {
+    name: 'Destroyer',
+    size: 2,
+    icon: 'D'
+  },
+  carrier: {
+    name: 'Aircraft Carrier',
+    size: 5,
+    icon: 'A'
+  }
+
+}
